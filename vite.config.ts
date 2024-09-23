a89b46f577713e681420e3a8ab8951c35331c789
Alvin0412 <wentai412@163.com>
2024-09-23 13:16:07 +0800

    display bug.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite";
- 
+
 export default defineConfig({
-  plugins: [react(),vue()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
